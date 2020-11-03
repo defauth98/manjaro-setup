59b7110b489ad9e257924861db2ccc23a4fa9568
defauth98 <neto.daniribeiro@gmail.com>
2020-11-03 17:00:04 -0300

    initial commit

---
+++ setup-manjaro.sh
@@ -0,0 +1,35 @@
+# TODO
+# Developer Tools 
+# - Terminal
+# - Zshell
+# - Vim
+# - VS code
+# - GIT
+# - Insominia
+# - Beekeper studio
+# - NVM
+# - yarn 
+# - Expo
+
+# Productivity tools
+
+# Inkscape
+# Gimp
+
+# Install tilix terminal
+yay -S tilix
+
+# Install jetbrains fonts
+yay -S ttf-jetbrains-mono
+
+# Install VS code
+yay -S visual-studio-code-bin
+
+# Install Github CLI
+yay -S github-cli
+
+# Install tmux
+yay -S tmux
+
+# Install insominia
+yay -S insomnia
