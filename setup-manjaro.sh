33d56267fa268e0d7961c3e08c2bb6520e37ea73
defauth98 <neto.daniribeiro@gmail.com>
2020-11-03 17:15:08 -0300

    Primeira versao

--- setup-manjaro.sh
+++ setup-manjaro.sh
@@ -11,25 +11,56 @@
 # - yarn 
 # - Expo
 
-# Productivity tools
+# Atualizar o sistema
+sudo pacman -Syyuu
 
-# Inkscape
-# Gimp
+# Instalar o YAY
+sudo pacman -S yay 
 
-# Install tilix terminal
-yay -S tilix
+# Install apps
+yay -S tilix ttf-jetbrains-mono visual-studio-code-bin github-cli tmux insomnia zsh git anki ulauncher beekeeper-studio-appimage brave inkscape gimp discord vlc
 
-# Install jetbrains fonts
-yay -S ttf-jetbrains-mono
+# baixar meu repositório de arquivos de configuração
+git clone https://github.com/defauth98/dotfiles.git dotfiles
 
-# Install VS code
-yay -S visual-studio-code-bin
+# Ir para o repositório
+cd dotfiles
 
-# Install Github CLI
-yay -S github-cli
+# Instale o Oh My Zshell
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# Install tmux
-yay -S tmux
+# Instale o tema spaceship
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
 
-# Install insominia
-yay -S insomnia
+# Crie um link simbólico para o tema
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+# Copie os arquivos de configuração do Zshell
+cp files/zshrc $HOME/.zshrc
+
+# Instale o vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Copie os arquivos de configuração do vim
+cp files/vimrc $HOME/.vimrc
+
+# Copie o arquivo de configuração para a sua home
+cp files/gitconfig $HOME/.gitconfig
+
+# Crie uma nova chave ssh
+ssh-keygen -t rsa -b 4096 -C "neto.daniribeiro@gmail.com"
+
+# Deixe rodando em background
+eval "$(ssh-agent -s)"
+
+#Adicione a sua lista de ssh keys
+ssh-add ~/.ssh/id_rsa
+
+# Mude o tipo de protocolo
+gh config set git_protocol ssh
+
+# Instale o tmux plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# Copie os arquivos de configução
+cp files/tmux.conf $HOME/.tmux.conf
