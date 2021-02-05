695cec5106a450607644621c6e37705e0f875b1f
defauth98 <neto.daniribeiro@gmail.com>
2021-02-05 14:44:29 -0300

    Colocando comentarios para demilitar sessoes e usando a varivel  na hora de copiar os arquivos

--- setup-manjaro.sh
+++ setup-manjaro.sh
@@ -1,43 +1,54 @@
-echo "Atualizando o sistema"
+#===============================================================================================================================================
+#                                              Atualiazação de sistema e instalação de apps
+#===============================================================================================================================================
+
 sudo pacman -Syyuu --noconfirm
 
-echo "Instalando o YAY!"
 sudo pacman -S yay base-devel --noconfirm
 
-echo "Install this apps -> tilix visual-studio-code-bin github-cli tmux insomnia zsh git anki ulauncher beekeeper-studio-appimage brave inkscape gimp discord vlc vim"
 yay -S tilix ttf-jetbrains-mono visual-studio-code-bin github-cli tmux insomnia zsh git anki ulauncher beekeeper-studio-appimage brave inkscape gimp discord vlc vim --noconfirm
+                                 
+#===============================================================================================================================================
+#                                              Configurando o zsh com Oh my zshell e o tema spaceship
+#===============================================================================================================================================
 
-
-echo "Baixando meu repositório de arquivos de configuração"
 git clone https://github.com/defauth98/dotfiles.git dotfiles
 
-echo "Entrando o repositório de arquivos de configuração"
-echo "Pasta atual:"
-pwd
-
-cd dotfiles
-echo "Pasta do dotfiles"
-pwd
-
-echo "Instalando o Oh My Zshell"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-cp files/zshrc $HOME/.zshrc
 
-echo "Instalando o Vundle e configurando o vim"
+cp $HOME/dotfiles/files/zshrc $HOME/.zshrc
+
+#===============================================================================================================================================
+#                                              Configurando vim com o tema x
+#===============================================================================================================================================
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp files/vimrc $HOME/.vimrc
 
-echo "Instalando o Github CLI"
-cp files/gitconfig $HOME/.gitconfig
+cp $HOME/dotfiles/files/vimrc $HOME/.vimrc
+
+#===============================================================================================================================================
+#                                              Configurando o git e o Github CLI
+#===============================================================================================================================================
+
+cp $HOME/dotfiles/files/gitconfig $HOME/.gitconfig
+
 ssh-keygen -t rsa -b 4096 -C "neto.daniribeiro@gmail.com"
+
 eval "$(ssh-agent -s)"
+
 ssh-add ~/.ssh/id_rsa
+
 gh config set git_protocol ssh
 
-echo "Configurando o tmux"
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-cp files/tmux.conf $HOME/.tmux.conf
+#===============================================================================================================================================
+#                                              Configurando o tmux e aplicando o tema dracula
+#===============================================================================================================================================
 
-echo "Done!"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+cp $HOME/dotfiles/files/tmux.conf $HOME/.tmux.conf
+
